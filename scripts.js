9997bae81f6e810fbdf4557095c087eb328a9df5
Shyla Earl <shyla.earl@gmail.com>
2020-10-01 14:54:52 -0500

    click button

--- scripts.js
+++ scripts.js
@@ -6,10 +6,17 @@ $(document).ready(onReady);
 
 function onReady(){ // 'let's get the page set up'
     console.log('hello from jQuery');
-    // add event listeners aka click events. 
-    // all click events will be in this function to connect to the DOM.
+    // add event listeners aka click events. All click events will be in this function to connect to the DOM.
     // get data to display on the page at loading
+    $('#clickMe').on('click', buttonClicked); // to select html elements for click events on DOM. This is essentially a jQuery object that has been written for us.
 }
+
+
+//write a function called buttonClicked
+function buttonClicked(){
+    console.log("I'm clicked!");
+}
+
 
 function hello(){
     console.log('hello!');
