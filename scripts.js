877f0f5d87ab2a297749d72b5126955b1aff5f2d
Shyla Earl <shyla.earl@gmail.com>
2020-10-02 12:03:00 -0500

    getters setters

--- scripts.js
+++ scripts.js
@@ -6,33 +6,39 @@ $(document).ready(onReady);
 
 let favFoods = ['pizza', 'donuts', 'tacos', 'tomato soup'];
 
+let employeeObject = [{}];
+
 function onReady(){ // 'let's get the page set up' anything you want on the DOM on load, the code should go in this function.
     console.log('hello from jQuery');
     // add event listeners aka click events. All click events will be in this function to connect to the DOM.
     // get data to display on the page at loading
     $('#clickMe').on('click', buttonClicked); // to select html elements for click events on DOM. This is essentially a jQuery object that has been written for us.
     
+    $('#submitForm').on('click', submitForm);
 
-    //can write following lines like this instead
-    //$('#favoriteFoods').append('<li>Hello</li>');
+    $('#favoriteFoods').on('click', '.deleteButton', deleteFunct);
+    
+    $('#favoriteFoods').on('click', '.changeColor', changeColor);
+}
 
-    let ulFoods = $('#favoriteFoods'); //(don't need this, but if you use it replace $('#favoriteFoods') with let ulFoods on line 16.
-    // ulFoods.append('<li>Hello</li>');
-    // ulFoods.append('<li>' + favFoods [0] + '</li>');
-    // ulFoods.append('<li>' + favFoods [1] + '</li>');
-    // ulFoods.append('<li>' + favFoods [2] + '</li>');
-    // ulFoods.append('<li>' + favFood [3] + '</li>');
-
-    //or you can do a for loop instead of having to hard code each new food item.
-
-    for(let i = 0; i < favFoods.length; i++){
-        //ulFoods.append(`<li> favFoods[i] </li>`); // with back ticks. can also be ('<li>' + favFoods[i] + '</li>')
-        ulFoods.append(`<li> one of my favorite foods is: ${favFoods[i]}! <button class='deleteButton'>DELETE</button></li>`);
-        //$('.deleteButton').on('click', deleteFunct);
+//write a function called buttonClicked
+function buttonClicked(){
+    console.log("I'm clicked!");
+    let ulFoods = $('#favoriteFoods');
+    for(let i = 0; i < favFoods.length; i ++){
+        ulFoods.append(`
+            <li> one of my favorite foods is: ${favFoods[i]}! 
+                <button class='deleteButton'>DELETE</button>
+                <button class='changeColor'>Change Color</button>
+            </li>`);
     }
-    // (`<li> One of my favorite foods is: ${favFoods[i]} </li>`) within backticks, this is a js identifier that indicates the value is a String. ${} is called string interpolation. 
+    
+}
 
-    $('.deleteButton').on('click', deleteFunct);
+
+function changeColor(){
+    console.log('Change color!');
+    $(this).parent().addClass('pinkColor');
 }
 
 function deleteFunct(){
@@ -43,11 +49,22 @@ function deleteFunct(){
     //.remove() = remmoves that element
 }
 
-//write a function called buttonClicked
-function buttonClicked(){
-    console.log("I'm clicked!");
-}
+function submitForm(){
+    console.log('in submit form');
+    let name = $('#name').val(); //get value with a getter. also setter .val(''); .text() is another getter/setter.
+    console.log(name); 
 
+    employeeObject.push(name);
+
+    $('#name').val('');
+
+    let title = $('#title').val();
+    console.log(title);
+
+    employeeObject.push(title);
+
+    $('#title').val('');
+}
 
 function hello(){
     console.log('hello!');
